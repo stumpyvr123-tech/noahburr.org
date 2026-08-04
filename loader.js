e57bfbc17935e982c19e27e26a9863bc477d12b8
nittany34 <stumpyvr123@gmail.com>
2026-08-04 14:38:54 -0400

    Add files via upload

--- loader.js
+++ loader.js
@@ -3,9 +3,12 @@ const loader = document.createElement("div");
 loader.id = "loading-screen";
 
 loader.innerHTML = `
-<svg viewBox="25 25 50 50">
-    <circle r="20" cy="50" cx="50"></circle>
-</svg>
+<div class="loader-content">
+    <svg viewBox="25 25 50 50">
+        <circle r="20" cy="50" cx="50"></circle>
+    </svg>
+    <p>Loading... Depending on your internet and device, this may take a while...</p>
+</div>
 `;
 
 document.addEventListener("DOMContentLoaded", () => {
