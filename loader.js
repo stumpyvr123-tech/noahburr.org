4109c536f5e16b8183f01bdd3b427f50f2740a5a
nittany34 <stumpyvr123@gmail.com>
2026-08-04 14:23:47 -0400

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,21 @@
+const loader = document.createElement("div");
+
+loader.id = "loading-screen";
+
+loader.innerHTML = `
+<svg viewBox="25 25 50 50">
+    <circle r="20" cy="50" cx="50"></circle>
+</svg>
+`;
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.prepend(loader);
+});
+
+window.addEventListener("load", () => {
+    loader.classList.add("fade-out");
+
+    setTimeout(() => {
+        loader.remove();
+    }, 300);
+});
