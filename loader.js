763af666649077456a1fa32d56ccac8af874b302
nittany34 <stumpyvr123@gmail.com>
2026-08-05 10:27:09 -0400

    Add files via upload

--- loader.js
+++ loader.js
@@ -6,6 +6,7 @@ loader.innerHTML = `
 <div class="loader-content">
     <svg viewBox="25 25 50 50">
         <circle r="20" cy="50" cx="50"></circle>
+        <body style="background-color:#B0E0E6"></body>
     </svg>
     <p>Loading... Depending on your internet and device, this may take a while...</p>
 </div>
